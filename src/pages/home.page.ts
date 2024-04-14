8af59ac874e1e44d59f38e5da632f1e99f8d889c
Natalia <nataliesparrow7@gmail.com>
2024-04-14 18:29:56 +0200

    chore: goto function added to home page

--- src/pages/home.page.ts
+++ src/pages/home.page.ts
@@ -2,7 +2,14 @@ import { MainMenuComponent } from '../components/main-menu.component';
 import { Page } from '@playwright/test';
 
 export class HomePage {
+  url = '';
+  homePageBanner = this.page.getByRole('img', { name: 'Banner' });
+
   constructor(private page: Page) {}
 
   mainMenu = new MainMenuComponent(this.page);
+
+  async goto(): Promise<void> {
+    await this.page.goto(this.url);
+  }
 }
