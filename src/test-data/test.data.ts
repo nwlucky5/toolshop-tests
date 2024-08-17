13384448a722c5816a66e00f5826305c48e10eab
Natalia <nataliesparrow7@gmail.com>
2024-08-17 14:07:14 +0200

    chore: test data added

---
+++ src/test-data/test.data.ts
@@ -0,0 +1,6 @@
+import { LoginUserModel } from '../models/user.model';
+
+export const testUser1: LoginUserModel = {
+  userEmail: 'customer@practicesoftwaretesting.com',
+  userPassword: 'welcome01',
+};
