f802552665dcd22fb186f26011ce6dc90f4f5ad0
Natalia <nataliesparrow7@gmail.com>
2024-08-17 14:10:37 +0200

    chore: login url update

--- src/pages/login.page.ts
+++ src/pages/login.page.ts
@@ -1,9 +1,16 @@
 import { MainMenuComponent } from '../components/main-menu.component';
+import { LoginUserModel } from '../models/user.model';
 import { Page } from '@playwright/test';
 
 export class LoginPage {
-  url = '/#/auth/login';
+  url = '/auth/login';
   loginTitle = this.page.getByRole('heading', { name: 'Login' });
+  emailInput = this.page.locator('[data-test="email"]');
+  passwordInput = this.page.locator('[data-test="password"]');
+  emailError = this.page.locator('[data-test="email-error"]');
+  passwordError = this.page.locator('[data-test="password-error"]');
+  loginError = this.page.locator('[data-test="login-error"]');
+  loginButton = this.page.locator('[data-test="login-submit"]');
 
   constructor(private page: Page) {}
 
@@ -11,5 +18,11 @@ export class LoginPage {
 
   async goto(): Promise<void> {
     await this.page.goto(this.url);
+  }
+
+  async login(loginUserData: LoginUserModel): Promise<void> {
+    await this.emailInput.fill(loginUserData.userEmail);
+    await this.passwordInput.fill(loginUserData.userPassword);
+    await this.loginButton.click();
   }
 }
