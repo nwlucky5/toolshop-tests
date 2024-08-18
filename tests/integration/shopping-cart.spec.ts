62eeb427f1c850f4a15b7de19f8b880709af3e07
Natalia <nataliesparrow7@gmail.com>
2024-08-18 16:22:18 +0200

    test: adding products to shopping cart test added

---
+++ tests/integration/shopping-cart.spec.ts
@@ -0,0 +1,69 @@
+import { HandTools } from '../../src/pages/hand-tools.page';
+import { Product } from '../../src/pages/product.page';
+import test, { expect } from '@playwright/test';
+
+test.describe('Adding product to shopping cart verification', () => {
+  let handToolsPage: HandTools;
+  let productPage: Product;
+
+  test.beforeEach(async ({ page }) => {
+    handToolsPage = new HandTools(page);
+    productPage = new Product(page);
+
+    await handToolsPage.goto();
+  });
+
+  test('add product to shopping cart with quantity equal to 1', async () => {
+    // Arrange
+    const expectedNotificationMessageText = 'Product added to shopping cart';
+
+    // Act
+    await handToolsPage.firstProduct.click();
+    await productPage.addToCart();
+
+    // Assert
+    await expect(productPage.notificationMessageText).toContainText(
+      expectedNotificationMessageText,
+    );
+  });
+
+  test('add product to shopping cart with quantity greater than 1', async () => {
+    // Arrange
+    const expectedNotificationMessageText = 'Product added to shopping cart';
+
+    // Act
+    await handToolsPage.firstProduct.click();
+    await productPage.addToCart('100');
+
+    // Assert
+    await expect(productPage.notificationMessageText).toContainText(
+      expectedNotificationMessageText,
+    );
+  });
+
+  test('reject adding product to shopping cart with negative quantity', async () => {
+    // Act
+    await handToolsPage.firstProduct.click();
+    await productPage.addToCart('-1');
+
+    // Assert
+    await expect(productPage.notificationMessageText).toBeHidden();
+  });
+
+  test('reject adding product to shopping cart with quantity equal to 0', async () => {
+    // Act
+    await handToolsPage.firstProduct.click();
+    await productPage.addToCart('0');
+
+    // Assert
+    await expect(productPage.notificationMessageText).toBeHidden();
+  });
+
+  test('reject adding out of stock product to shopping cart', async () => {
+    // Act
+    await handToolsPage.productOutOfStock.click();
+
+    // Assert
+    await expect(productPage.addToCartButton).toBeDisabled();
+  });
+});
