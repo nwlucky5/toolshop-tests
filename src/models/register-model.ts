221e0b6d8abfb6010aead7779629d58469adc852
Natalia <nataliesparrow7@gmail.com>
2024-09-13 00:47:03 +0200

    feat: register model added

---
+++ src/models/register-model.ts
@@ -0,0 +1,13 @@
+export interface RegisterUserModel {
+  first_name: string;
+  last_name: string;
+  address: string;
+  city: string;
+  state: string;
+  country: string;
+  postcode: string;
+  phone: string;
+  dob: string;
+  password: string;
+  email: string;
+}
