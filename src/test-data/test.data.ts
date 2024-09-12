3279d3e31a8dd2dffad39acf11f4aa1dc943b96e
Natalia <nataliesparrow7@gmail.com>
2024-09-13 00:40:36 +0200

    chore: admin user added

--- src/test-data/test.data.ts
+++ src/test-data/test.data.ts
@@ -1,6 +1,11 @@
 import { LoginUserModel } from '../models/user.model';
 
 export const testUser1: LoginUserModel = {
-  userEmail: 'customer@practicesoftwaretesting.com',
+  userEmail: 'customer2@practicesoftwaretesting.com',
+  userPassword: 'welcome01',
+};
+
+export const testUser2: LoginUserModel = {
+  userEmail: 'admin@practicesoftwaretesting.com',
   userPassword: 'welcome01',
 };
