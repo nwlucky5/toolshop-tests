8771a591cca2e004992ff797dbbd2cd30539f12a
Natalia <nataliesparrow7@gmail.com>
2024-09-13 00:43:23 +0200

    chore: navigation functions added

--- src/components/main-menu.component.ts
+++ src/components/main-menu.component.ts
@@ -1,8 +1,6 @@
 import { Page } from '@playwright/test';
 
 export class MainMenuComponent {
-  constructor(private page: Page) {}
-
   homePageLink = this.page.locator('[data-test="nav-home"]');
   categorySubmenu = this.page.locator('[data-test="nav-categories"]');
   handToolsLink = this.page.locator('[data-test="nav-hand-tools"]');
@@ -12,5 +10,49 @@ export class MainMenuComponent {
   rentalsPageLink = this.page.locator('[data-test="nav-rentals"]');
   contactLink = this.page.locator('[data-test="nav-contact"]');
   loginPageLink = this.page.locator('[data-test="nav-sign-in"]');
+  shoppingCartIcon = this.page.locator('[data-test="nav-cart"]');
   userSubmenu = this.page.locator('[data-test="nav-menu"]');
+
+  constructor(private page: Page) {}
+
+  async clickHomePageLink(): Promise<void> {
+    await this.homePageLink.click();
+  }
+
+  async clickHandToolsLink(): Promise<void> {
+    await this.categorySubmenu.click();
+    await this.handToolsLink.click();
+  }
+
+  async clickPowerToolsLink(): Promise<void> {
+    await this.categorySubmenu.click();
+    await this.powerToolsLink.click();
+  }
+
+  async clickOtherPageLink(): Promise<void> {
+    await this.categorySubmenu.click();
+    await this.otherPageLink.click();
+  }
+
+  async clickSpecialToolsPageLink(): Promise<void> {
+    await this.categorySubmenu.click();
+    await this.specialToolsPageLink.click();
+  }
+
+  async clickRentalsLink(): Promise<void> {
+    await this.categorySubmenu.click();
+    await this.rentalsPageLink.click();
+  }
+
+  async clickContactLink(): Promise<void> {
+    await this.contactLink.click();
+  }
+
+  async clickLoginLink(): Promise<void> {
+    await this.loginPageLink.click();
+  }
+
+  async clickShoppingCartIcon(): Promise<void> {
+    await this.shoppingCartIcon.click();
+  }
 }
