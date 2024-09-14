e2091b7ef1dee1d4c6e45c3e023a947b7ea7c821
Natalia <nataliesparrow7@gmail.com>
2024-09-14 20:31:41 +0200

    feat: global setup added

---
+++ src/global-setup.ts
@@ -0,0 +1,10 @@
+import { STORAGE_STATE } from '../playwright.config';
+import * as fs from 'fs';
+
+async function globalSetup(): Promise<void> {
+  if (fs.existsSync(STORAGE_STATE)) {
+    fs.unlinkSync(STORAGE_STATE);
+  }
+}
+
+export default globalSetup;
