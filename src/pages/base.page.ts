c7fb5e3e9d41d5763ae392a0ac5df5a73854c65d
Natalia <nataliesparrow7@gmail.com>
2024-09-22 00:55:04 +0200

    feat: base page added

---
+++ src/pages/base.page.ts
@@ -0,0 +1,19 @@
+import { Page } from '@playwright/test';
+
+export class BasePage {
+  url = '';
+  constructor(protected page: Page) {}
+
+  async goto(parameters = ''): Promise<void> {
+    await this.page.goto(`${this.url}${parameters}`);
+  }
+
+  async getTitle(): Promise<string> {
+    await this.page.waitForLoadState();
+    return await this.page.title();
+  }
+
+  async waitForPageToLoadUrl(): Promise<void> {
+    await this.page.waitForURL(this.url);
+  }
+}
