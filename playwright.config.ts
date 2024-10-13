c0d947e4cbfd61820e718bd43ca5dd4ec957d11f
Natalia <nataliesparrow7@gmail.com>
2024-10-13 21:01:21 +0200

    chore: update reporters

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,12 @@ export default defineConfig({
   fullyParallel: true,
   retries: 0,
   workers: undefined,
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['github'],
+    ['json', { outputFile: 'results.json' }],
+    ['junit', { outputFile: 'results.xml' }],
+  ],
   use: {
     baseURL: BASE_URL,
     actionTimeout: 0,
